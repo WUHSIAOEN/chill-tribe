6af237e66012397a1eb9ea8db8072e449570fe44
HsiaoEn <crystal071112@gmail.com>
2025-02-01 13:40:35 +0800

    add activity image java

--- src/main/java/web/activity/service/ActivityService2.java
+++ src/main/java/web/activity/service/ActivityService2.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import web.activity.vo.Activity2;
 //import web.activity.vo.ActivityImage;
+import web.activity.vo.ActivityImage;
 
 public interface ActivityService2 {
-	// 首頁查詢所有活動
+		// 首頁查詢所有活動
 		List<Activity2> searchAll();
 		
 		// 首頁查詢所有活動圖片
-//		List<ActivityImage> searchAllActivityImages();
+		List<ActivityImage> searchAllActivityImages();
 }
