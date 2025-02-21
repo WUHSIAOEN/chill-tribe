838cc2834098d5500399cd0d2562d5ce719e98d4
David <abbalacafe1974@gmail.com>
2025-02-22 00:10:57 +0800

    add get products

--- src/main/java/web/shoppingcart/dao/ShoppingCartDao.java
+++ src/main/java/web/shoppingcart/dao/ShoppingCartDao.java
@@ -5,6 +5,10 @@ import java.util.List;
 import web.shoppingcart.vo.ShoppingCart;
 
 public interface ShoppingCartDao {
-	// 新增商品到購物車，回傳整數
+	// 新增商品到購物車
 	List<ShoppingCart> insertToCart(ShoppingCart shoppingCart);
+	// 選擇多個商品
+	List<ShoppingCart> selectAllCart();
+	// 刪除單個珊品
+	int deleteCart(Integer id);
 }
--- src/main/java/web/shoppingcart/service/ShoppingCartService.java
+++ src/main/java/web/shoppingcart/service/ShoppingCartService.java
@@ -7,4 +7,6 @@ import web.shoppingcart.vo.ShoppingCart;
 public interface ShoppingCartService {
 	// 加入購物車
 	List<ShoppingCart> AddToCart(ShoppingCart shppingCart);
+	// 找到所有產品
+	List<ShoppingCart> findAllProducts();
 }
