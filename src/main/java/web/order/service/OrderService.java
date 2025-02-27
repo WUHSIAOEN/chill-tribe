f4c68b705728879af59a889046a2d426fafaa47a
HsiaoEn <crystal071112@gmail.com>
2025-02-27 10:17:00 +0800

    add order insert service 20%

--- src/main/java/web/order/service/OrderService.java
+++ src/main/java/web/order/service/OrderService.java
@@ -4,6 +4,8 @@ import web.order.vo.Orders;
 
 public interface OrderService {
 	
-	String placeOrder(Orders order);
+	String placeOrderWithPayment(Orders order);
+	
+	String placeOrderWithoutPayment(Orders order);
 	
 }
