c5f589aef6095575ee8a64594059971ffc66205c
Ernie <tibameernie@gmail.com>
2025-03-06 20:14:41 +0800

    update SupplierRegister supplier is not null run sendEmail

--- src/main/java/web/supplier/controller/SupplierRegisterController.java
+++ src/main/java/web/supplier/controller/SupplierRegisterController.java
@@ -58,6 +58,7 @@ public class SupplierRegisterController {
 		
 		supplier = service.register(supplier);
 		
+		if (supplier != null) {
 		// 呼叫SendEmail
 		SendEmail sendEmail = new SendEmail();
 		
@@ -74,6 +75,7 @@ public class SupplierRegisterController {
 				"<html>" + "<head></head>" + "<body>" + "<h1>歡迎註冊Chill Tribe!</h1>"
 				+ "<p><a href='http://localhost:8080/chill-tribe/supplier/supplierlogin.html'> 請點擊此連結已進行供應商登入 </a></p>" + "</body>" + "</html>";
 		sendEmail.send(sesv2Client, sender, recipient, subject, bodyHTML);
+		}
 		return supplier;
 //	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
