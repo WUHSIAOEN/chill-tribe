6f656c2c7c8e86cbd0f3572bfacada52914e6b1c
Ernie <tibameernie@gmail.com>
2025-03-15 00:55:28 +0800

    add logout is OK.

--- src/main/java/web/member/controller/LogoutController.java
+++ src/main/java/web/member/controller/LogoutController.java
@@ -1,9 +1,15 @@
 package web.member.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.SessionStatus;
+
+import core.vo.GoogleData;
 
 
 // 一般會員登出
@@ -11,8 +17,18 @@ import org.springframework.web.bind.support.SessionStatus;
 @RequestMapping({"member/logout", "/logout"})
 public class LogoutController  {
 	
-	@GetMapping
-	protected void login(SessionStatus sessionStatus) {
-		sessionStatus.setComplete();
+	@DeleteMapping
+	protected void logout(SessionStatus sessionStatus, HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession(false);
+		GoogleData gd = (GoogleData) session.getAttribute("Google");
+		System.out.println("執行登出");
+		System.out.println("第一次"+session);
+		System.out.println("第一次"+gd);
+//		sessionStatus.setComplete();
+		session.invalidate();
+		session = request.getSession(false);
+//		gd = (GoogleData) session.getAttribute("Google");
+		System.out.println("第二次"+session);
+		System.out.println("第二次"+gd);
 	}
 }
