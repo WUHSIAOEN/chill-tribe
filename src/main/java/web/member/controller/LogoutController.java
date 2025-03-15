005e40b3e0fb634fd00412c90cc8541dbcc87fe3
Ernie <tibameernie@gmail.com>
2025-03-15 12:58:50 +0800

    Update logout

--- src/main/java/web/member/controller/LogoutController.java
+++ src/main/java/web/member/controller/LogoutController.java
@@ -1,5 +1,6 @@
 package web.member.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,24 +12,28 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import core.vo.GoogleData;
 
-
 // 一般會員登出
 @RestController
-@RequestMapping({"member/logout", "/logout"})
-public class LogoutController  {
-	
+@RequestMapping({ "member/logout", "/logout" })
+public class LogoutController {
+
 	@DeleteMapping
 	protected void logout(SessionStatus sessionStatus, HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession(false);
 		GoogleData gd = (GoogleData) session.getAttribute("Google");
 		System.out.println("執行登出");
-		System.out.println("第一次"+session);
-		System.out.println("第一次"+gd);
-//		sessionStatus.setComplete();
+		System.out.println("第一次" + session);
+		System.out.println("第一次" + gd);
+		sessionStatus.setComplete();
 		session.invalidate();
 		session = request.getSession(false);
-//		gd = (GoogleData) session.getAttribute("Google");
-		System.out.println("第二次"+session);
-		System.out.println("第二次"+gd);
+		Cookie cookie = new Cookie("JSESSIONID", null);
+		cookie.setPath("/chill-tribe");
+		cookie.setMaxAge(0);
+		cookie.setSecure(true);
+		response.addCookie(cookie);
+		response.setHeader("Set-Cookie", "JSESSIONID=; Path=/chill-tribe; Max-Age=0; HttpOnly; Secure; SameSite=Lax");
+		System.out.println("第二次" + session);
+		System.out.println("第二次" + gd);
 	}
 }
