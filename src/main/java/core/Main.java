0852ffff7711e372eb9fe7bfc939a0197dbc8d7b
HsiaoEn <crystal071112@gmail.com>
2025-03-17 00:38:48 +0800

    remove OAuth source code & modify OAuth related controller from hard code credential to call aws secrets manager api & Create a util method (getOAuthKeyValue) in GetSecretValue.java

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 import javax.naming.NamingException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import core.util.GetSecretValue;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import web.activity.dao.ActivitySearchDao;
 import web.activity.dao.impl.ActivitySearchDaoImpl;
 import web.activity.service.impl.ActivityServiceImpl2;
@@ -18,41 +26,9 @@ import web.order.vo.Orders;
 public class Main {
 
 	public static void main(String[] args) throws NamingException {
-		// 測試回傳
-//		List<ActivityImage> activityImages = new ActivitySearchDaoImpl().selectActivityImageById(1);
-//		for(ActivityImage activityImage : activityImages) {
-//			System.out.println(activityImage.getImageBase64());
-//		}
-//		ActivityServiceImpl2 as2 = new ActivityServiceImpl2();
-//		as2.searchActivityByStart();
-		
-		
-//		List<IndexActivityCard> indexActivityCards = new ActivitySearchDaoImpl().selectByNameCatgoryRegion("", "運動", "北部");
-//		for(IndexActivityCard indexActivityCard : indexActivityCards) {
-//			System.out.println(indexActivityCard.getActivityName());
-//		}
-		
-//		ActivityServiceImpl2 as2 = new ActivityServiceImpl2();
-//		List<Activities> cards = as2.searchActivityByFilter("健行", "all", "all");
-//		for(Activities card : cards) {
-//			System.out.println(card.getActivityName());
-//		}
-		
-		OrderDao Od = new OrderDaoImpl();
-		Orders order = new Orders();
-		order.setActivityId(1);
-		order.setMemberId(1);
-		order.setQuantity(4);
-		order.setOrderStatus("no_payment_required");
-		order.setPaymentMethod("none");
-		order.setOrderContact("小名");
-		order.setContactMail("aaabbbcc@gmail.com");
-		order.setContactPhone("456196582");
-		order.setRequirement("無");
-		Od.insert(order);
-		
-		
-	}	
+
+		String clientId = GetSecretValue.getOAuthKeyValue("CLIENT_ID");
+		System.out.println(clientId);
+
+	}
 }
-
-
--- src/main/java/web/controller/OauthController2.java
+++ src/main/java/web/controller/OauthController2.java
@@ -10,6 +10,8 @@ import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.gson.JsonObject;
 
+import core.util.GetSecretValue;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +22,10 @@ import java.util.Set;
 
 @WebServlet("/anotherOauthLogin")
 public class OauthController2 extends AbstractAuthorizationCodeServlet {
-    private static final String CLIENT_ID = "778188269055-9htm2tsvh4b4m1eaeul5jj9qe9eig0j0.apps.googleusercontent.com";
-    private static final String CLIENT_SECRET = "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";
+	private static final long serialVersionUID = 1L;
+	
+	private static final String CLIENT_ID = GetSecretValue.getOAuthKeyValue("CLIENT_ID");
+    private static final String CLIENT_SECRET = GetSecretValue.getOAuthKeyValue("CLIENT_SECRET");
     private static final java.io.File DATA_STORE_DIR = new java.io.File("credentials");
     private static final DataStoreFactory DATA_STORE_FACTORY;
 
