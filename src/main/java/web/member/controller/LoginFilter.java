bbf51738ac32f53dd4447d4cda22da5ab894bd6d
Ernie <tibameernie@gmail.com>
2025-03-17 22:16:09 +0800

    Add Member Filter is OK.

--- src/main/java/web/member/controller/LoginFilter.java
+++ src/main/java/web/member/controller/LoginFilter.java
@@ -3,6 +3,8 @@
 ////import java.io.IOException;
 //
 //import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.ServletException;
 ////import javax.servlet.ServletException;
 ////import javax.servlet.ServletRequest;
 ////import javax.servlet.ServletResponse;
@@ -15,15 +17,32 @@
 //import web.member.vo.Member;
 //
 //// Filter限制 除了login
-////@WebFilter("/member/*")
+//@WebFilter("/member/*")
 //public class LoginFilter extends HttpFilter {
+//	
 //	private static final long serialVersionUID = 1L;
 //	
 //	@Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+////        System.out.println("Filter被初始化時，Web容器會⾃動呼叫此⽅法，因為Filtet支援的是3.1版後，但是我們加了Google的API只有2.4版不支援所以要創一個初始化");
+//    }
+//	
+//	@Override
 //	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) {
+//		
 //		try {
+//			
 //			if (req.getServletPath().startsWith("/member/login") ||
-//				req.getServletPath().startsWith("/member/register")) {
+//				req.getServletPath().startsWith("/member/register") ||
+//				req.getServletPath().startsWith("/member/header") ||
+//				req.getServletPath().startsWith("/member/footer") ||
+//				req.getServletPath().startsWith("/member/css/index.css") ||
+//				req.getServletPath().startsWith("/member/js/member-register.js") ||
+//				req.getServletPath().startsWith("/member/js/loginmodal.js") ||
+//				req.getServletPath().startsWith("/member/js/index-ernie.js") ||
+//				req.getServletPath().equals("/chilltribe") ||
+//				req.getServletPath().equals("/"))
+//			{
 //				chain.doFilter(req, res);
 //			}	else {
 //				HttpSession session = req.getSession();
@@ -40,4 +59,9 @@
 //			e.printStackTrace();
 //		}
 //	}
+//	
+//	@Override
+//    public void destroy() {
+////        System.out.println("Filter被銷毀時，Web容器會⾃動呼叫此⽅法，因為Filtet支援的是3.1版後，但是我們加了Google的API只有2.4版不支援所以要創一個銷毀");
+//    }
 //}
